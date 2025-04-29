5a3292d20ca45c897e1838838e5bb2df03d2b8e4
rachittt06 <racguptta06@gmail.com>
2025-04-29 13:54:42 +0530

    Update st.js

--- st.js
+++ st.js
@@ -1,23 +1,18 @@
 let cart = [];
 let total = 0;
-
 function addToCart(item, price) {
   cart.push({ item, price });
   total += price;
   updateCart();
 }
-
-function updateCart() {
+function updateCart(){
   const cartList = document.getElementById('cart');
   const totalElement = document.getElementById('total');
   cartList.innerHTML = '';
-  
-  
   cart.forEach((product, index) => {
     const li = document.createElement('li');
     li.textContent = ${product.item} - $${product.price.toFixed(2)};
     cartList.appendChild(li);
   });
-
   totalElement.textContent = total.toFixed(2);
 }
